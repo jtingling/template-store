6d2bac959deebf9ba4cbbcb7c2bddc38600fe257
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-21 17:42:56 -0400

    Unused code, testing landing page

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect } from 'react';
 import { getAllProducts, getProductCollections } from '../adapters/index'
-
-export async function getStaticProps() {
-  const products = await getAllProducts();
-  return {
-      props: {
-          products,
-      },
-  }
-}
-
-export default function Home({products}) {
+export default function Home() {
 
   return (
     <div className={styles.container}>
       <main>
         <div>
+          <Link href="/landing/"><a>Landing</a></Link><br/>
           <Link href="/shoes/"><a>shoes</a></Link><br/>
           <Link href="/paintings/"><a>paintings</a></Link><br/>
           <Link href="/accessories/"><a>accessories</a></Link>
