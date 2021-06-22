21dbcbbe477032950b865947a9963a6430b8cd85
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-22 16:44:44 -0400

    redirect to landing page

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
     images: {
         domains: ["cdn.shopify.com", "images.unsplash.com"],
     },
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/landing',
+            permanent: true,
+          },
+        ]
+      },
 }
