7aed7f364f8d7a874dd95609ee3b1df763743e6b
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-22 16:44:25 -0400

    Centered call to action

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -16,7 +16,7 @@ export default function Hero({ blurHash, image }) {
     if (process.browser) {
         useEffect(() => {
             if (document.children[0].clientHeight < 568) {
-                setHeight(document.children[0].clientHeight * .90)
+                setHeight(document.children[0].clientHeight)
             } else {
                 setHeight(document.children[0].clientHeight)
             }
@@ -29,26 +29,32 @@ export default function Hero({ blurHash, image }) {
     useEffect(() => {
         setTimeout(() => {
             setLoadImg(true)
-        }, 2000)
+        }, 2500)
     }, [])
 
     return (
-        <div className="position-relative">
-            <div className="position-absolute">
-                <Fade in={!loadImg} timeout={1000} unmountOnExit={true} onExiting={() => setHero(true)}>
-                    <Blurhash hash={blurHash} resolutionX={32} resolutionY={32} width={(width)} height={(height * .90)} />
+        <div>
+            <div>
+                <div>
+                <Fade in={!loadImg} unmountOnExit={true} onExiting={() => setHero(true)}>
+                    <Blurhash hash={blurHash} resolutionX={32} resolutionY={32} width={(width)} height={(height)} />
                 </Fade>
-                <Fade in={loadImg && hero} timeout={1000}>
-                    <Image src={(image + `&w=${width}` + `&h=${height}`)} width={width} height={(height * .90)} priority={true} priority={true} />
+                </div>
+                <div>
+                <Fade in={loadImg && hero} timeout={2000}>
+                    <Image src={(image + `&w=${width}` + `&h=${height}`)} width={width} height={(height)} priority={true} priority={true} />
                 </Fade>
+                </div>
             </div>
             <div className="position-absolute top-50 start-50 translate-middle">
-                <Container>
-                    <Row className="flex-column text-center">
-                        <Col><h1>Welcome to "Your ShopName here"!</h1></Col>
-                        <Col className="text-center"><Button>Shop Shoes</Button></Col>
-                    </Row>
-                </Container>
+                <Fade in={loadImg && hero} timeout={2000}>
+                    <Container>
+                        <Row className="flex-column text-center">
+                            <Col><h1>Welcome to "Your ShopName here"!</h1></Col>
+                            <Col className="text-center"><Button>Shop Shoes</Button></Col>
+                        </Row>
+                    </Container>
+                </Fade>
             </div>
         </div>
     )
