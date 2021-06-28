bba28d1bd3fff4c645d31b05a488c6ac88028902
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-28 16:55:48 -0400

    added permanent components

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css'
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <NavBar/>
+    <Component {...pageProps} />
+    <Footer/>
+  </>)
 }
 
 export default MyApp
