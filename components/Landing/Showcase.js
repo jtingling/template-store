2d2288a9251bf2eff51c4d72aa11ea40df9bbb51
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-28 16:53:36 -0400

    generic component for displaying featured products

--- components/Landing/Showcase.js
+++ components/Landing/Showcase.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
-import { getProductByType  } from '../Products/bl';
+import { getProductByType } from '../Products/bl';
 import Layout from '../Products/Layout';
 
-export default function Showcase() {
-    const [ product, setProduct] = useState(false);
+export default function Showcase({productType}) {
+    const [product, setProduct] = useState([]);
 
-    useEffect( async ()=>{
-        const response = await getProductByType("ACCESSORIES", 3);
-        setProduct(response);
-    },[])
+    useEffect(() => {
+        const getProducts = async () => {
+            const response = await getProductByType(productType.toUpperCase());
+            setProduct(response)
+        }
+        getProducts();
+    }, [])
     return (
-        <Layout products={product} category="accessories"/>
+        <div>
+            <h2 className="text-center">Stylish {productType}</h2>
+            {
+                product.length !== 0 ? <Layout products={product} category="accessories" /> : <></>
+            }
+        </div>
     )
 }
