a8512471d9b6c4e9ae817e4ac8df39041e58f158
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-28 16:54:38 -0400

    modified layout

--- components/Products/Layout.js
+++ components/Products/Layout.js
@@ -1,13 +1,15 @@
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Product from './Product'
 export default function Layout({ products, category }) {
+    const [layoutItems, setLayoutItems] = useState(products);
     return (
-        <Container fluid>
+        <Container >
             <Row className='row justify-content-center'>
                 {
-                    products.map((product) => {
-                     return <Product product={product} category={category} />
+                    layoutItems.map((item) => {
+                     return <Product product={item} category={category} />
                     })
                 }
             </Row>
