2b1a220ddb04b273d3efbc0095a2e55fa85a691e
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-30 16:59:21 -0400

    updated function names

--- adapters/index.js
+++ adapters/index.js
@@ -1,5 +1,5 @@
-export { client, unsplash } from './auth'
-export { createCheckout, getCheckout, setCheckout } from './Checkout/checkout'
+export { client, unsplash, getClient } from './auth'
+export { getCheckout, getCartIdFromStorage, initCheckout } from './Checkout/checkout'
 export { addItemToCart, removeItemFromCart, updateCart } from '../adapters/Checkout/lineItems'
 export { getAllProducts, getSingleProduct, getProductByHandle } from '../adapters/Products/products'
 export { getProductCollections, getCollectionById } from '../adapters/Products/collections'
