8ee12b315ba36df86a9dbf1be00aedf84bb0ad39
Jacob Tingling <jacob.tingling@protonmail.com>
2021-06-30 16:53:43 -0400

    cart ribbon added to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,25 @@
 import '../styles/App.css'
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
-
+import Ribbon from '../components/Ribbon'
+import { useEffect, useState } from 'react';
+import { getCheckout, initCheckout } from '../adapters/index';
 function MyApp({ Component, pageProps }) {
+  const [ cart, setCart ] = useState(null)
+  //TODO: add context for Cart so ribbon stays updated without re rendering
+  useEffect(()=>{
+    (async ()=> {
+      await initCheckout()
+      const checkout = await getCheckout(window.localStorage.getItem("cart"));
+      setCart(checkout)
+    })()
+
+},[])
   return (
   <>
-    <NavBar/>
-    <Component {...pageProps} />
+    <NavBar/> {    console.log(cart)}
+    <Component {...pageProps}  />
+    {cart !== null && <Ribbon cart={cart}/>}
     <Footer/>
   </>)
 }
