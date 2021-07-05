c42bb1a95e8e03b082f55f2938533e99181aec70
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-05 16:37:18 -0400

    Added responsive menu.

---
+++ components/Navs/NavBar.js
@@ -0,0 +1,10 @@
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '/public/footer-img.png'
+import Link from 'next/link'
+import Nav from 'react-bootstrap/Nav'
+import Menu from './Menu'
+export default function NavBar() {
+    return (
+        <Menu type="Navbar"/>
+    )
+}
--- components/Navs/Ribbon.js
+++ components/Navs/Ribbon.js
@@ -1,35 +1,24 @@
-import { useState, useEffect, useContext } from 'react';
-import { getCartIdFromStorage, getCheckout } from "../../adapters/index";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar';
-import { CartContext } from '../../contexts/App'
 import Menu from './Menu';
+import Link from 'next/link'
 
-export default function Ribbon() {
-    const idContext = useContext(CartContext);
-    const [ checkout, setCheckout ] = useState(null)
-    const [ clientCheckout, setClientCheckout] = useState(idContext.cartChange)
-    useEffect( async()=>{
-        setClientCheckout(idContext.cartChange)
-        let checkout = await getCheckout(idContext.cartId)
-        setCheckout(checkout);
+export default function Ribbon( { checkout }) {
 
-    }, [idContext.cartChange])
     return (
-        <> {console.log(checkout)}
+        <>
             {
-                clientCheckout[0].lineItems.length !== 0 ?
+                checkout !== 0 ?
                     <Container className="fixed-bottom mw-100 bg-dark text-white p-2">
                         <Row className="w-50 text-center align-items-center m-auto">
 
                             <Col><Menu type="Ribbon" /></Col>
-                            <Col><Navbar.Text>{clientCheckout[0].lineItems.length} items</Navbar.Text></Col>
-                            <Col><Navbar.Text>${clientCheckout[0].lineItemsSubtotalPrice.amount}</Navbar.Text></Col>
-                            <Col><Button variant='success'><Navbar.Text>CHECKOUT</Navbar.Text></Button></Col>
+                            <Col><Navbar.Text><Link href='/cart'><a>{checkout.lineItems.length} items</a></Link></Navbar.Text></Col>
+                            <Col><Navbar.Text>${checkout.lineItemsSubtotalPrice.amount}</Navbar.Text></Col>
+                            <Col><Button variant='success' onClick={()=> window.location.replace(checkout.webUrl)}><Navbar.Text>CHECKOUT</Navbar.Text></Button></Col>
                         </Row>
                     </Container> :
                     <></>
