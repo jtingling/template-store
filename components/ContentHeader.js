55ca869ae4d6f6efe8c3cd4addf82e764faad8e8
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-05 16:42:16 -0400

    changed element height modifier

--- components/ContentHeader.js
+++ components/ContentHeader.js
@@ -7,7 +7,7 @@ export default function ContentHeader({image}) {
     return (
         <div className='position-relative'>
             {
-                image !== null && <Image src={image} width={width} height={(height * .75)} layout='responsive'/>
+                image !== null && <Image src={image} width={width} height={(height * .33)} layout='responsive'/> // mutiplier to adjust height
             }
         </div>
     )
