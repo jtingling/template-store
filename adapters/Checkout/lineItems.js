ed6ed5d310759bd9b5404e64acac683a4a6405a1
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-05 16:43:26 -0400

    Updated checkout setters to return updated items.

--- adapters/Checkout/lineItems.js
+++ adapters/Checkout/lineItems.js
@@ -1,4 +1,4 @@
-import {client} from '../index'
+import {client, getCheckout } from '../index'
 
 const addItemToCart = async (checkoutId, items) => {
 
@@ -17,6 +17,7 @@ const addItemToCart = async (checkoutId, items) => {
         ]
     }]
     await client.checkout.addLineItems(checkoutId, lineItemsToAdd);
+    return await getCheckout(checkoutId);
 }
 
 const removeItemFromCart = async (checkoutId, items) => {
@@ -24,9 +25,20 @@ const removeItemFromCart = async (checkoutId, items) => {
     return `${items.length} item(s) removed to cart.`
 }
 
-const updateCart = async (checkoutId, items) => {
-    await client.checkout.updateLineItems(checkoutId, items);
-    return `Cart updated.`
+const updateCart = async (event, checkoutId, itemId) => {
+    event.preventDefault();
+
+    const lineItemsToAdd = [{
+        id: itemId,
+        quantity: parseInt(event.target.value, 10)
+    }]
+    if (event.target != 0) {
+        await client.checkout.updateLineItems(checkoutId, lineItemsToAdd);
+    } else if (event.target === 0) {
+        await client.checkout.removeItemFromCart(checkoutId, itemId);
+    }
+
+    return await getCheckout(checkoutId);
 }
 
 export { addItemToCart, removeItemFromCart, updateCart };
