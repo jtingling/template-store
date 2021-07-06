df79586b61970e22b48a32ed6004127ad0524f85
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-06 13:31:50 -0400

    Added error handling

--- components/Products/ProductDescription.js
+++ components/Products/ProductDescription.js
@@ -1,3 +1,4 @@
+import Alert from 'react-bootstrap/Alert'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -6,7 +7,7 @@ import Button from 'react-bootstrap/Button'
 import Collapse from 'react-bootstrap/Collapse'
 import Image from 'next/image'
 import { useState, useEffect, useContext } from 'react'
-import { addItemToCart, getCartIdFromStorage, getCheckout} from '../../adapters/index';
+import { addItemToCart, getCartIdFromStorage, getCheckout } from '../../adapters/index';
 import { CartContext } from '../../contexts/App/index';
 
 export default function ProductDescription({ product }) {
@@ -16,112 +17,130 @@ export default function ProductDescription({ product }) {
     const [isHOpen, setIsHOpen] = useState(false)
     const [cartId, setCartId] = useState(null)
     const [item, setItem] = useState(null)
+    const [isError, setIsError] = useState(false);
     const checkoutContext = useContext(CartContext);
 
-    useEffect(()=>{
-        (async ()=> {
+    function displayError() {
+        setTimeout(() => {
+            setIsError(false)
+        }, 4000)
+    }
+
+    useEffect(() => {
+        (async () => {
             const id = await getCartIdFromStorage()
             setCartId(id);
         })();
-    },[])
+    }, [])
     return (
-        <Container fluid className="mx-0 px-0">
-            <Row className="d-flex flex-column flex-sm-row">
-                <Col>
-                    <Carousel fade >
-                        {
-                            product.images.map((image) => {
-                                return <Carousel.Item key={image.id} interval={2000} >
-                                    <Image src={image.src} width={450} height={350} layout='responsive' alt={image.altText} />
-                                </Carousel.Item>
-                            })
-                        }
-                    </Carousel>
-                </Col>
-                <Col>
-                    <Row>
-                        <Col ><h1 >{product.title}</h1></Col>
-                    </Row>
-                    <Row xs={2} className='mt-3'>
-                        <Col className='m-1'><span className="display-3">${product.variants[0].price}</span></Col>
-                        <Col className='m-1'><span className="display-5"><strong>Shipping calculated at checkout</strong></span></Col>
-                        <Col className='m-1'><p className="display-4">Size</p></Col>
-                    </Row>
-                    <Row sm={product.options[0].values}>
-                        {
-                            product.options[0].values.map((size, idx) => {
-                                return (
-                                    <Col className="row justify-content-center">
-                                        <Button variant="outline-dark" className="w-50" onClick={()=> setItem(product.variants[idx])}>{size.value}</Button>
-                                    </Col>
+        <>
+            <Container fluid className="mx-0 px-0 mt-5">
+                <Row className="d-flex flex-column flex-sm-row">
+                    <Col>
+                        <Carousel fade >
+                            {
+                                product.images.map((image) => {
+                                    return <Carousel.Item key={image.id} interval={2000} >
+                                        <Image src={image.src} width={450} height={350} layout='responsive' alt={image.altText} />
+                                    </Carousel.Item>
+                                })
+                            }
+                        </Carousel>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Col ><h1>{product.title}</h1></Col>
+                        </Row>
+                        <Row xs={2} className='mt-3'>
+                            <Col className='m-1'><span className="display-3">${product.variants[0].price}</span></Col>
+                            <Col className='m-1'><span className="display-7"><strong>Shipping calculated at checkout</strong></span></Col>
+                            <Col className='m-1'><p className="display-5">Size</p></Col>
+                        </Row>
+                        <Row sm={product.options[0].values}>
+                            {
+                                product.options[0].values.map((size, idx) => {
+                                    return (
+                                        <Col className="row justify-content-center">
+                                            <Button variant="outline-dark" className="w-50" onClick={() => setItem(product.variants[idx])}>{size.value}</Button>
+                                        </Col>
+                                    )
+                                }
                                 )
                             }
-                            )
-                        }
-                    </Row>
-                    <Row sm={1} className="d-flex flex-column flex-sm-row w-xs-5 w-75 mx-auto">
-                        <Button variant='light' className="my-3 py-3 px-0 mx-0" 
-                            onClick={ async ()=> {
-                                const clientCheckout = await addItemToCart(cartId, item);
-                                checkoutContext.setCheckout(clientCheckout)
-                            }}>
-                        Add To Cart</Button>
-                        <Button variant='dark' className="py-3 px-0 m-0" onClick={()=> getCheckout(cartId).webUrl}>Buy It Now</Button>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Row sm={1} className="d-flex flex-column flex-sm-row w-75 mx-auto mt-5">
-                                <Col>
-                                    <h3 onClick={() => setIsDOpen(!isDOpen)} className='border-bottom border-dark'>Description</h3>
-                                    <Collapse in={isDOpen}>
-                                        <div>
+                        </Row>
+                        <Row sm={1} className="d-flex flex-column flex-sm-row w-xs-5 w-75 mx-auto">
+                            <Button variant='light' className="my-3 py-3 px-0 mx-0"
+                                onClick={async () => {
+                                    let clientCheckout = 0;
+                                    try {
+                                        clientCheckout = await addItemToCart(cartId, item);
+                                    } catch (e) {
+                                        setIsError(true)
+                                    }
+                                    if (clientCheckout !== 0) {
+                                        checkoutContext.setCheckout(clientCheckout)
+                                    }
+                                }}>
+                                Add To Cart</Button>
+                            <Button variant='dark' className="py-3 px-0 m-0" onClick={() => getCheckout(cartId).webUrl}>Buy It Now</Button>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Row sm={1} className="d-flex flex-column flex-sm-row w-75 mx-auto mt-5">
+                                    <Col>
+                                        <h3 onClick={() => setIsDOpen(!isDOpen)} className='border-bottom border-dark'>Description</h3>
+                                        <Collapse in={isDOpen}>
+                                            <div>
+                                                <p>
+                                                    {product.descriptionHtml}
+                                                </p>
+                                            </div>
+                                        </Collapse>
+                                    </Col>
+                                    <Col>
+                                        <h3 onClick={() => setIsSOpen(!isSOpen)} className='border-bottom border-dark'>Sizing Chart</h3>
+                                        <Collapse in={isSOpen}>
+                                            <div>
+                                                <p>
+                                                    <Image src="/public/shirt_sizing_chart_480x480_12bf5e0b-48cd-40e5-8c04-bbb74429b713_480x480.png" width="200" height="200" />
+                                                </p>
+                                            </div>
+                                        </Collapse>
+                                    </Col>
+                                    <Col>
+                                        <h3 onClick={() => setIsCOpen(!isCOpen)} className='border-bottom border-dark'>Care Instructions</h3>
+                                        <Collapse in={isCOpen}>
+                                            <div>
+                                                <ul>
+                                                    <li>Lorem Ipsum</li>
+                                                    <li>dolor sit amet</li>
+                                                    <li>consectetur adipiscing elit</li>
+                                                </ul>
+                                            </div>
+                                        </Collapse>
+                                    </Col>
+                                    <Col>
+                                        <h3 onClick={() => setIsHOpen(!isHOpen)} className='border-bottom border-dark'>How this was Made</h3>
+                                        <Collapse in={isHOpen}>
                                             <p>
-                                                {product.descriptionHtml}
+                                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                                culpa qui officia deserunt mollit anim id est laborum.
                                             </p>
-                                        </div>
-                                    </Collapse>
-                                </Col>
-                                <Col>
-                                    <h3 onClick={() => setIsSOpen(!isSOpen)} className='border-bottom border-dark'>Sizing Chart</h3>
-                                    <Collapse in={isSOpen}>
-                                        <div>
-                                            <p>
-                                                <Image src="/public/shirt_sizing_chart_480x480_12bf5e0b-48cd-40e5-8c04-bbb74429b713_480x480.png" width="200" height="200" />
-                                            </p>
-                                        </div>
-                                    </Collapse>
-                                </Col>
-                                <Col>
-                                    <h3 onClick={() => setIsCOpen(!isCOpen)} className='border-bottom border-dark'>Care Instructions</h3>
-                                    <Collapse in={isCOpen}>
-                                        <div>
-                                            <ul>
-                                                <li>Lorem Ipsum</li>
-                                                <li>dolor sit amet</li>
-                                                <li>consectetur adipiscing elit</li>
-                                            </ul>
-                                        </div>
-                                    </Collapse>
-                                </Col>
-                                <Col>
-                                    <h3 onClick={() => setIsHOpen(!isHOpen)} className='border-bottom border-dark'>How this was Made</h3>
-                                    <Collapse in={isHOpen}>
-                                        <p>
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                            culpa qui officia deserunt mollit anim id est laborum.
-                                        </p>
-                                    </Collapse>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+                                        </Collapse>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+            {<Alert className='text-center display-6' show={isError} key={product.id} variant='warning'>Please select a size</Alert>}
+            {displayError()}
 
+        </>
     )
 }
