34daae95ca06f2cfa34073386124fe99e755f295
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-06 13:35:20 -0400

    Removed image scaling modifier

--- pages/accessories/index.js
+++ pages/accessories/index.js
@@ -4,10 +4,9 @@ import { getProductByType } from '../../components/Products/bl';
 import Layout from '../../components/Products/Layout'
 import ContentHeader from '../../components/ContentHeader';
 import { useClientWidth, useClientHeight } from '../../components/hooks';
+
 export async function getStaticProps() {
-    const accessories = await getProductByType("ACCESSORIES")
-    
-    const response = await unsplash.photos.getRandom({ collectionIds: ["936583"] });
+    const accessories = await getProductByType("ACCESSORIES")    const response = await unsplash.photos.getRandom({ collectionIds: ["936583"] });
     const heroImg = await response.response.urls.raw;
     return {
         props: {
@@ -20,7 +19,7 @@ export async function getStaticProps() {
 export default function Paintings ({ accessories, heroImg }) {
     const width = useClientWidth();
     const height = useClientHeight();
-    const image = heroImg + `&fit=crop&q=50&dpr=1&w=${width}&h=${(height/4)}`
+    const image = heroImg + `&fit=crop&q=50&dpr=1&w=${width}&h=${height}`
     return (
         <div>
             <Head>
