f97ed0cfda52c9f95676c35a6a23cbd3ce59595c
Jacob Tingling <jacob.tingling@protonmail.com>
2021-07-07 13:32:38 -0400

    Added styling.

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -3,8 +3,6 @@ import Fade from 'react-bootstrap/Fade'
 import { useState, useEffect } from 'react'
 import { Blurhash } from 'react-blurhash';
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import { useClientHeight, useClientWidth } from '../hooks';
 
@@ -30,17 +28,17 @@ export default function Hero({ blurHash, image }) {
                 </div>
                 <div >
                     <Fade in={loadImg && hero} timeout={2000}>
-                        <Image src={(image + `&w=${width}` + `&h=${height}`)} width={width} height={(height)} priority={true}/>
+                        <Image src={(image + `&w=${width}` + `&h=${height}`)} width={width} height={(height)} priority={true} />
                     </Fade>
                 </div>
             </div>
             <div className="position-absolute top-50 start-50 translate-middle" >
                 <Fade in={loadImg && hero} timeout={2000}>
                     <Container>
-                        <Row className="flex-column text-center">
-                            <Col><h1>Welcome to "Your ShopName here"!</h1></Col>
-                            <Col className="text-center"><Button>Shop Shoes</Button></Col>
-                        </Row>
+                        <div className="text-center bg-dark text-white p-4 rounded opacity-75 ">
+                            <h1 className='opacity-100'>Welcome to "Your ShopName here"!</h1>
+                            <Button className='mt-3 opacity-100'>Shop Shoes</Button>
+                        </div>
                     </Container>
                 </Fade>
             </div>
