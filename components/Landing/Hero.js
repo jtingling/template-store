9446db134f0f5eff93e548a9f1b1dc3b1dac3c6d
Jacob Tingling <jtingling@live.com>
2022-03-28 14:14:54 -0400

    Prettier formatting changes.

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -1,47 +1,57 @@
-import Image from 'next/image'
-import Fade from 'react-bootstrap/Fade'
-import { useState, useEffect } from 'react'
-import { Blurhash } from 'react-blurhash';
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
-import { useClientHeight, useClientWidth } from '../hooks';
+import Image from "next/image";
+import Fade from "react-bootstrap/Fade";
+import { useState, useEffect } from "react";
+import { Blurhash } from "react-blurhash";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { useClientHeight, useClientWidth } from "../hooks";
 
 export default function Hero({ blurHash, image }) {
-    const [hero, setHero] = useState(false)
-    const [loadImg, setLoadImg] = useState(false)
-    const width = useClientWidth();
-    const height = useClientHeight();
+  const [hero, setHero] = useState(false);
+  const [loadImg, setLoadImg] = useState(false);
+  const width = useClientWidth();
+  const height = useClientHeight();
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoadImg(true)
-        }, 2500)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadImg(true);
+    }, 3500);
+  }, []);
 
-    return (
+  return (
+    <div>
+      <div>
         <div>
-            <div>
-                <div>
-                    <Fade in={!loadImg} unmountOnExit={true} onExiting={() => setHero(true)}>
-                        <Blurhash hash={blurHash} resolutionX={32} resolutionY={32} width={(width)} height={(height)} />
-                    </Fade>
-                </div>
-                <div >
-                    <Fade in={loadImg && hero} timeout={2000}>
-                        <Image src={(image + `&w=${width}` + `&h=${height}`)} width={width} height={(height)} priority={true} />
-                    </Fade>
-                </div>
-            </div>
-            <div className="position-absolute top-50 start-50 translate-middle" >
-                <Fade in={loadImg && hero} timeout={2000}>
-                    <Container>
-                        <div className="text-center bg-dark text-white p-4 rounded opacity-75 ">
-                            <h1 className='opacity-100'>Welcome to "Your ShopName here"!</h1>
-                            <Button className='mt-3 opacity-100'>Shop Shoes</Button>
-                        </div>
-                    </Container>
-                </Fade>
-            </div>
+          <Fade
+            in={!loadImg}
+            unmountOnExit={true}
+            onExiting={() => setHero(true)}
+          >
+            <Blurhash
+              hash={blurHash}
+              resolutionX={32}
+              resolutionY={32}
+              width={width}
+              height={height}
+            />
+          </Fade>
         </div>
-    )
+        <div>
+          <Fade in={loadImg && hero} timeout={2000}>
+            <Image src={image} width={width} height={height} priority={true} />
+          </Fade>
+        </div>
+      </div>
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <Fade in={loadImg && hero} timeout={2000}>
+          <Container>
+            <div className="text-center bg-dark text-white p-4 rounded opacity-75 ">
+              <h1 className="opacity-100">Welcome to "Your ShopName here"!</h1>
+              <Button className="mt-3 opacity-100">Shop Shoes</Button>
+            </div>
+          </Container>
+        </Fade>
+      </div>
+    </div>
+  );
 }
--- components/hooks.js
+++ components/hooks.js
@@ -1,29 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-function useClientWidth () {
-    const [width, setWidth] = useState(0);
-    useEffect(()=>{
-        if (typeof window !== undefined) {
-            setWidth(window.innerWidth);
-        } else {
-            throw "Unable to detect DOM."
-        }
-    }, [])
-    
-    return width;
+function useClientWidth() {
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setWidth(window.innerWidth);
+    } else {
+      throw "Unable to detect DOM.";
+    }
+  }, []);
+
+  return width;
 }
 
-function useClientHeight () {
-    const [height, setHeight] = useState(0)
-    useEffect(()=>{
-        if (typeof window !== undefined) {
-            setHeight(window.innerHeight)
-        } else {
-            throw "Unable to detect DOM."
-        }
-    },[])
-    
-    return height;
+function useClientHeight() {
+  const [height, setHeight] = useState(0);
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setHeight(window.innerHeight);
+    } else {
+      throw "Unable to detect DOM.";
+    }
+  }, []);
+
+  return height;
 }
 
-export {useClientHeight, useClientWidth};
+export { useClientHeight, useClientWidth };
