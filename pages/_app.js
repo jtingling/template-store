3ef5e6e26f36f726a2f164093573744112bd0bc7
Jacob Tingling <jtingling@live.com>
2022-03-31 17:20:25 -0400

    Refactored checkout.

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,43 @@
 import "../styles/App.css";
+import { SSRProvider } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navs/NavBar";
 import Ribbon from "../components/Navs/Ribbon";
 import { CartContext } from "../contexts/App";
 import { getCartIdFromStorage, getCheckout } from "../adapters";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  const [cartId, setCartId] = useState(0);
   const [checkout, setCheckout] = useState({ lineItems: [] });
   const router = useRouter();
+
+  const initCheckout = async () => {
+    const id = await getCartIdFromStorage();
+    const clientCheckout = await getCheckout(id);
+    setCheckout(clientCheckout);
+  };
+
   useEffect(() => {
-    (async () => {
-      const id = await getCartIdFromStorage();
-      setCartId(id);
-    })();
+    initCheckout();
   }, []);
-  useEffect(() => {
-    (async () => {
-      const clientCheckout = await getCheckout(cartId);
-      setCheckout(clientCheckout);
-    })();
-  }, [cartId]);
-  useEffect(() => {
-    setCheckout(checkout);
-  }, [checkout.lineItems]);
 
   return (
-    <main className="vh-100">
-      <NavBar />
-      <CartContext.Provider
-        value={{
-          cartId: cartId,
-          setCheckout: setCheckout,
-          checkout: checkout,
-        }}
-      >
-        <Component {...pageProps} className="mw-100" />
-        {router.pathname !== "/cart" && <Ribbon checkout={checkout} />}
-      </CartContext.Provider>
-      <Footer />
-    </main>
+    <SSRProvider>
+      <main className="overflow-hidden">
+        <NavBar />
+        <CartContext.Provider
+          value={{
+            setCheckout: setCheckout,
+            checkout: checkout,
+          }}
+        >
+          <Component {...pageProps} />
+          {router.pathname !== "/cart" && <Ribbon checkout={checkout} />}
+        </CartContext.Provider>
+        <Footer />
+      </main>
+    </SSRProvider>
   );
 }
 
