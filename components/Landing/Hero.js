f368b6fd8566a67dc33fdd8f2372a9b6315147c1
Jacob Tingling <jtingling@live.com>
2022-03-31 17:18:51 -0400

    Changed Image component type

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import React from "react";
+import Image from "react-bootstrap/Image";
 import Link from "next/link";
 import Fade from "react-bootstrap/Fade";
 import { useState, useEffect } from "react";
@@ -39,18 +40,20 @@ export default function Hero({ blurHash, image }) {
         </div>
         <div>
           <Fade in={loadImg && hero} timeout={2000}>
-            <Image src={image} width={width} height={height} priority={true} />
+            <Image src={image} width={width} height={height} />
           </Fade>
         </div>
       </div>
-      <div className="position-absolute top-50 start-50 translate-middle">
+      <div className="position-absolute top-50 start-50 translate-middle w-100">
         <Fade in={loadImg && hero} timeout={2000}>
           <Container>
             <div className="text-center bg-dark text-white p-4 rounded opacity-75 ">
-              <h1 className="opacity-100">Welcome to "Your ShopName here"!</h1>
+              <h1>Welcome to "Your ShopName here"!</h1>
               <Link href="/shoes">
                 <a>
-                  <Button className="mt-3 opacity-100">Shop Shoes</Button>
+                  <Button className="mt-3 opacity-100" variant="secondary">
+                    Shop Shoes
+                  </Button>
                 </a>
               </Link>
             </div>
