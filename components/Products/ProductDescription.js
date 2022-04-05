d263ed562e0e0893fd4e4b18d7cabe25f53dd8cf
Jacob Tingling <jtingling@live.com>
2022-04-05 10:31:08 -0400

    Refactored and reduced component footprint.

--- components/Products/ProductDescription.js
+++ components/Products/ProductDescription.js
@@ -1,3 +1,4 @@
+import { useState, useContext, useEffect } from "react";
 import Alert from "react-bootstrap/Alert";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -5,27 +6,52 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
-import Collapse from "react-bootstrap/Collapse";
 import Image from "next/image";
-import { useState, useContext } from "react";
+import ProductDetail from "./ProductDetail";
 import { addItemToCart, buyNow } from "../../adapters/index";
 import { CartContext } from "../../contexts/App/index";
-import sizingChart from "/public/shirt_sizing_chart_480x480_12bf5e0b-48cd-40e5-8c04-bbb74429b713_480x480.png";
 
 export default function ProductDescription({ product }) {
-  const [isDOpen, setIsDOpen] = useState(false);
-  const [isSOpen, setIsSOpen] = useState(false);
-  const [isCOpen, setIsCOpen] = useState(false);
-  const [isHOpen, setIsHOpen] = useState(false);
-  const [cartId, setCartId] = useState(null);
   const [item, setItem] = useState(null);
   const [isError, setIsError] = useState(false);
   const checkoutContext = useContext(CartContext);
-  function displayError() {
-    setTimeout(() => {
-      setIsError(false);
-    }, 4000);
-  }
+  let timer;
+
+  const renderCarousel = () => {
+    return (
+      <Carousel fade className="mw-100" controls={false}>
+        {product.images.map((image) => {
+          return (
+            <Carousel.Item key={image.id} interval={2000}>
+              <Image
+                src={image.src}
+                width={450}
+                height={350}
+                layout="responsive"
+                alt={image.altText}
+              />
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    );
+  };
+
+  const renderSizes = () => {
+    return product.options[0].values.map((size, idx) => {
+      return (
+        <Col key={size.value} className="row justify-content-center gy-1 ">
+          <Button
+            variant="outline-dark"
+            className="w-auto p-1"
+            onClick={() => setItem(product.variants[idx])}
+          >
+            {size.value}
+          </Button>
+        </Col>
+      );
+    });
+  };
 
   async function handleClick() {
     let clientCheckout = 0;
@@ -33,29 +59,25 @@ export default function ProductDescription({ product }) {
       clientCheckout = await addItemToCart(checkoutContext.checkout.id, item);
       checkoutContext.setCheckout(clientCheckout);
     } catch (e) {
-      setIsError(true);
+      timer = setTimeout(() => {
+        setIsError(true);
+      }, 2000);
     }
   }
+
+  useEffect(() => {
+    timer = setTimeout(() => {
+      setIsError(false);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isError]);
+
   return (
     <Container fluid className="mt-5 p-0">
       <Row className="d-flex flex-column flex-sm-row ">
-        <Col className="mt-1 p-0">
-          <Carousel fade className="mw-100" controls={false}>
-            {product.images.map((image) => {
-              return (
-                <Carousel.Item key={image.id} interval={2000}>
-                  <Image
-                    src={image.src}
-                    width={450}
-                    height={350}
-                    layout="responsive"
-                    alt={image.altText}
-                  />
-                </Carousel.Item>
-              );
-            })}
-          </Carousel>
-        </Col>
+        <Col className="mt-1 p-0">{renderCarousel()}</Col>
         <Col>
           <Row>
             <Col>
@@ -80,22 +102,7 @@ export default function ProductDescription({ product }) {
             sm={product.options[0].values}
             className="justify-content-center m-0 p-0"
           >
-            {product.options[0].values.map((size, idx) => {
-              return (
-                <Col
-                  key={size.value}
-                  className="row justify-content-center gy-1 "
-                >
-                  <Button
-                    variant="outline-dark"
-                    className="w-50 px-0"
-                    onClick={() => setItem(product.variants[idx])}
-                  >
-                    {size.value}
-                  </Button>
-                </Col>
-              );
-            })}
+            {renderSizes()}
           </Row>
           <Row sm={1} className="d-flex flex-column flex-sm-row w-75 mx-auto">
             <Button
@@ -119,79 +126,12 @@ export default function ProductDescription({ product }) {
               Buy It Now
             </Button>
           </Row>
-          <Row>
+          <Row
+            sm={1}
+            className="d-flex flex-column flex-sm-row w-75 mx-auto mt-5"
+          >
             <Col>
-              <Row
-                sm={1}
-                className="d-flex flex-column flex-sm-row w-75 mx-auto mt-5"
-              >
-                <Col>
-                  <h3
-                    onClick={() => setIsDOpen(!isDOpen)}
-                    className="border-bottom border-dark"
-                  >
-                    Description
-                  </h3>
-                  <Collapse in={isDOpen}>
-                    <div>{product.descriptionHtml}</div>
-                  </Collapse>
-                </Col>
-                <Col>
-                  <h3
-                    onClick={() => setIsSOpen(!isSOpen)}
-                    className="border-bottom border-dark"
-                  >
-                    Sizing Chart
-                  </h3>
-                  <Collapse in={isSOpen}>
-                    <div>
-                      <Image
-                        src={sizingChart}
-                        width="460"
-                        height="450"
-                        layout="responsive"
-                        alt="sizing chart"
-                      />
-                    </div>
-                  </Collapse>
-                </Col>
-                <Col>
-                  <h3
-                    onClick={() => setIsCOpen(!isCOpen)}
-                    className="border-bottom border-dark"
-                  >
-                    Care Instructions
-                  </h3>
-                  <Collapse in={isCOpen}>
-                    <div>
-                      <ul>
-                        <li>Lorem Ipsum</li>
-                        <li>dolor sit amet</li>
-                        <li>consectetur adipiscing elit</li>
-                      </ul>
-                    </div>
-                  </Collapse>
-                </Col>
-                <Col>
-                  <h3
-                    onClick={() => setIsHOpen(!isHOpen)}
-                    className="border-bottom border-dark"
-                  >
-                    How this was Made
-                  </h3>
-                  <Collapse in={isHOpen}>
-                    <p>
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-                  </Collapse>
-                </Col>
-              </Row>
+              <ProductDetail product={product} />
             </Col>
           </Row>
         </Col>
@@ -206,7 +146,6 @@ export default function ProductDescription({ product }) {
           Please select a size
         </Alert>
       </Navbar>
-      {displayError()}
     </Container>
   );
 }
