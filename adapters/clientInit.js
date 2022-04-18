d70307cd9ba0ea8b25fd78399c57f28e231a4d61
Jacob Tingling <jtingling@live.com>
2022-04-18 14:14:01 -0400

    Fixed env variable name

--- adapters/clientInit.js
+++ adapters/clientInit.js
@@ -2,11 +2,11 @@ import Client from "shopify-buy";
 import { createApi } from "unsplash-js";
 
 const client = Client.buildClient({
-  domain: `${process.env.SHOPIFY_DOMAIN}`,
-  storefrontAccessToken: `${process.env.SHOPIFY_ACCESS}`,
+  domain: `${process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN}`,
+  storefrontAccessToken: `${process.env.NEXT_PUBLIC_SHOPIFY_ACCESS_TOKEN}`,
 });
 const unsplash = createApi({
-  accessKey: `${process.env.UNSPLASH_AK}`,
+  accessKey: `${process.env.NEXT_PUBLIC_UNSPLASH_AK}`,
 });
 
 export { client, unsplash };
